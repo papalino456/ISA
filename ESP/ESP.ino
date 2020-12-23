12371aaf171bdc15ad388453f4633da8d2b647bd
papalino456 <sebastian.barrio.b@gmail.com>
2020-12-22 18:32:40 -0600

    add sensor reading

---
+++ ESP/ESP.ino
@@ -0,0 +1,42 @@
+#include "DallasTemperature.h"
+#include "OneWire.h"
+
+//pin variables
+humSensor = A1;
+tempSensor = 12;
+
+//variables setup
+float tempVal;
+float humVal;
+
+//Temperature sensor setup --DS18B20--
+OneWire sensor(tempSensor);
+DallasTemperature temperature(&sensor);
+//------------------------------------
+
+void setup(){
+
+  //Humidity sensor
+  pinMode(humSensor, INPUT);
+
+  //Temperature sensor
+  pinMode(tempSensor, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop(){
+
+  // Get temperature values
+  temperature.requestTemperatures();
+  tempVal = temperature.getTempCByIndex(0);
+  
+  // Get humidity values
+  humVal = analogRead(A1);
+
+  // Print to serial port
+  Serial.print("Temperature: ");
+  Serial.println(tempVal);
+  Serial.print("Humidity: ");
+  Serial.println(humVal);
+}
