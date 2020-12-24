3627e0bbac0bfa7f24410b1568effdbabe8b4c7c
papalino456 <sebastian.barrio.b@gmail.com>
2020-12-24 14:41:46 -0600

    add conectivity and data sending to esp32

--- ESP/ESP.ino
+++ ESP/ESP.ino
@@ -1,13 +1,20 @@
-#include "DallasTemperature.h"
-#include "OneWire.h"
+#include <HTTPClient.h>
+#include <DallasTemperature.h>
+#include <OneWire.h>
+#include <WiFi.h>
 
 //pin variables
-humSensor = A1;
-tempSensor = 12;
+const int humSensor = 35;
+const int tempSensor = 12;
 
 //variables setup
 float tempVal;
 float humVal;
+
+//connection variables
+const char* ssid = "HALL9000";
+const char* password = "ANIROC1966";
+const char* server = "172.69.170.138:1234";
 
 //Temperature sensor setup --DS18B20--
 OneWire sensor(tempSensor);
@@ -23,6 +30,14 @@ void setup(){
   pinMode(tempSensor, INPUT);
 
   Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println(".");
+  }
+  Serial.print("connected!, ip:");
+  Serial.println(WiFi.localIP());
+  
 }
 
 void loop(){
@@ -32,12 +47,38 @@ void loop(){
   tempVal = temperature.getTempCByIndex(0);
   
   // Get humidity values
-  humVal = analogRead(A1);
+  humVal = analogRead(humSensor);
 
   // Print to serial port
   Serial.print("Temperature: ");
   Serial.println(tempVal);
   Serial.print("Humidity: ");
   Serial.println(humVal);
+
+  //Send to server via http post command
+  if(WiFi.status() == WL_CONNECTED) {
+    //begin conection to server
+    HTTPClient http;
+    http.begin(server);
+
+    //specify content type
+    http.addHeader("Content-Type","text/plain");
+
+    //data send
+    int httpResCode = http.POST("Hello World!!!!1");
+
+    //await response code (200 ok, 404 not found, etc...)
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResCode);
+
+    //release resources
+    http.end();
+  }
+  else {
+    Serial.println("not connected");
+  }
+
+  delay(1000);
+  
 }
  
