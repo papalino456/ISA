261305826c800ccd3b6cdce112bfc693bf0c6455
papalino456 <sebastian.barrio.b@gmail.com>
2021-06-14 18:22:17 -0500

    First AI model

--- ESP/AI.py
+++ ESP/AI.py
@@ -1,1 +1,12 @@
-import tensorflow
+import tensorflow.keras as kr
+
+def createModel():
+    model = kr.Sequential()
+
+    model.add(kr.layers.Dense(3,activation="relu"))
+    model.add(kr.layers.Dense(9,activation="relu"))
+    model.add(kr.layers.Dense(1,activation="sigmoid"))
+
+    model.compile(loss="mse", optimizer=kr.optimizers.SGD(lr=0.01), metrics="acc")
+
+    model.fit(X,Y, epochs=250)
